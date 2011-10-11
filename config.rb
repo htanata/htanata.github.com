f4915abe909433675f0f8cdf867f514098b78f14
Hendy Tanata <htanata@gmail.com>
2011-10-11 16:59:38 +0800

    Regenerate compass config.rb.

--- config.rb
+++ config.rb
@@ -1,5 +1,6 @@
-require 'html5-boilerplate'
 # Require any additional compass plugins here.
+require 'html5-boilerplate'
+
 
 # Set this to the root of your project when deployed:
 http_path = "/"
@@ -7,6 +8,18 @@ css_dir = "css"
 sass_dir = "_stylesheets"
 images_dir = "images"
 javascripts_dir = "js"
+
+output_style = :compressed
+
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
-output_style = :compressed
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
