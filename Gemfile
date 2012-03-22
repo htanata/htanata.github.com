393ee5b93846528b0fd04e3b67363496b479977c
Hendy Tanata <htanata@gmail.com>
2012-03-22 13:32:12 +0800

    Switch to Twitter Bootstrap.

--- Gemfile
+++ Gemfile
@@ -2,4 +2,4 @@ source 'http://rubygems.org'
 
 gem 'jekyll', '~>0.11.0'
 gem 'compass', '~>0.11.5'
-gem 'html5-boilerplate', '~>1.0.0'
+gem 'bootstrap-sass', '~>2.0.1'
--- config.rb
+++ config.rb
@@ -1,5 +1,5 @@
 # Require any additional compass plugins here.
-require 'html5-boilerplate'
+require 'bootstrap-sass'
 
 
 # Set this to the root of your project when deployed:
